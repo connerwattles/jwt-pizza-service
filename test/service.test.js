262d16a0c95d465f684b9ede1e7d3e14a615d47a
connerwattles <connerwattles@gmail.com>
2024-10-04 11:34:29 -0600

    Updated for 86 percent coverage without db errors

--- test/service.test.js
+++ test/service.test.js
@@ -1,13 +1,23 @@
 const request = require("supertest");
 const app = require("../src/service.js");
 
+// Mock the database module
+jest.mock("../src/database/database.js", () => {
+  return {
+    getOrder: jest.fn(), // Assuming you have a getOrder function in your database module
+    addMenuItem: jest.fn(),
+    addUser: jest.fn(),
+  };
+});
+
+const dbModel = require("../src/database/database.js");
+
 describe("GET /", () => {
-  it("responds with a welcome message and the version", async () => {
+  it("responds with welcome message", async () => {
     const response = await request(app).get("/");
+    console.log(response.body);
     expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body.message).toBe("welcome to JWT Pizza");
+    expect(response.body).toHaveProperty("message", "welcome to JWT Pizza");
     expect(response.body).toHaveProperty("version");
-    expect(response.body.version).toBe("20240518.154317");
   });
 });
